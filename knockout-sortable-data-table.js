b09442ee2ced110b24a29c35383b510612403165
Victor Alekseev <victor.alekseev.stash@arcadia.spb.ru>
2015-12-04 20:56:22 +0300

    remove jquery dependency add some display features

--- knockout-sortable-data-table.js
+++ knockout-sortable-data-table.js
@@ -8,10 +8,10 @@
     var proto = {},
         sorter = function(prop, direction) {
             var sortBy = typeof prop === 'function'
-                        ? prop
-                        : prop != null
-                            ? function (item) { return item[prop]; }
-                            : function (item) { return item; },
+                             ? prop
+                             : prop != null
+                             ? function(item) { return item[prop]; }
+                             : function(item) { return item; },
                 compare = function(a, b) {
                     a = sortBy(a);
                     b = sortBy(b);
@@ -21,15 +21,41 @@
             return direction === 'desc'
                        ? function(a, b) { return compare(b, a); }
                        : compare;
+        },
+        extend = function() {
+            var args = arguments;
+            return Array.prototype.reduce.call(args, ko.utils.extend);
+        },
+        isString = function(value) {
+            return typeof value === 'string' || value instanceof String;
         };
 
-    function TableColumn(column) {
+    var defaultSettings = {
+        columns: [],
+        sortable: false,
+        throttle: 100,
+        compareBy: 'id',
+        comparator: null,
+        page: {
+            sizes: [10, 25, 50, 100],
+            size: 100,
+            index: 0,
+            radius: 2
+        },
+        css: {
+            sort: { '': '', 'asc': 'dt-sort-down', 'desc': 'dt-sort-up' }
+        }
+    };
 
-        this.name = column.name || '';
+    function TableColumn(column) {
+        var stringValue = isString(column.value) ? column.value : '';
+
+        this.name = column.name || stringValue;
         this.value = column.value;
         this.template = column.template;
+        this.html = column.html;
         this.sortable = column.sortable;
-        this.sortField = column.sortField;
+        this.sortBy = column.sortBy || column.value || null;
         this.width = column.width;
         this.cssClass = ko.observable('');
     }
@@ -37,7 +63,7 @@
     function Page(page) {
 
         this.sizes = ko.observable(page.sizes || []);
-        this.sizes.selected = ko.observable(page.size && (!this.sizes().length || this.sizes().indexOf(page.size) >= 0) ? page.size : ko.utils.arrayFirst(this.sizes()) || 0);
+        this.sizes.selected = ko.observable(page.size && (!this.sizes().length || this.sizes().indexOf(page.size) >= 0) ? page.size : this.sizes()[0] || 0);
 
 
         this.index = ko.observable(page.index || 0);
@@ -80,7 +106,7 @@
         }, this);
     }
 
-    ko.utils.extend(Page.prototype, {
+    extend(Page.prototype, {
         prev: function() {
             var index = this.index();
             if (index > 0) {
@@ -100,50 +126,48 @@
         }
     });
 
-    function DataTable(config) {
-        config.columns = [];
-        this.config = config;
-        this.sortable = config.sortable || false;
-        this.throttle = config.throttle || 100;
-        this.loader = config.loader || this._staticLoader;
-        this.selectedItem = ko.observableArray();
-        this.items = ko.observableArray(config.items || []);
-        this.columns = [];
-        for (var i = 0; i < config.columns.length; i++) {
-            var column = config.columns[i];
-            column.sortable = column.sortable || this.sortable;
-            this.columns.push(new TableColumn(column));
-        }
+    function DataTable(settings) {
+        this._settings = extend({}, defaultSettings, settings);
+
+        var columns = this._settings.columns;
+        columns = columns && isString(columns) ? columns.split(',') : Array.isArray(columns) ? columns : [];
+
+        this.loader = settings.loader || this._staticLoader;
+
+        this.items = ko.observableArray(ko.unwrap(this._settings.items) || []);
+        this.items.selected = ko.observable();
+        this.columns = columns.map(this._createColumnDefenition, this);
+        this.columns.htmlTemplate = ko.utils.parseHtmlFragment('<div data-bind="html: $data"></div>');
         this.sorting = { sortColumn: null, sortOrder: '' };
-        this.comparator = config.comparator || this._defaultComparator;
+        this.comparator = settings.comparator || this._defaultComparator;
 
-        this.page = new Page(ko.utils.extend({
-            sizes: [2, 4, 5, 10],
-            size: 4
-        }, this.config.page || {}));
+        this.page = new Page(extend({}, defaultSettings.page, this._settings.page));
 
         
-
-        this.content = ko.computed(this.reload, this).extend({ throttle: this.throttle });
-
-        this.cssMap = { '': '', 'asc': 'sortDown', 'desc': 'sortUp' };
         
-        for (var m in proto) {
-            if (proto.hasOwnProperty(m)) {
-                this[m] = this[m].bind(this);
-            }
-        }
-        this.reload();
+        this.initialize();
     }
 
-    ko.utils.extend(DataTable.prototype, proto = {
+    extend(DataTable.prototype, proto = {
+        initialize: function() {
+            ko.computed(this.reload, this).extend({ throttle: this._settings.throttle });
+
+            this.cssMap = defaultSettings.css.sort;
+
+            for (var m in proto) {
+                if (proto.hasOwnProperty(m)) {
+                    this[m] = this[m].bind(this);
+                }
+            }
+            this.reload();
+        },
 
         reload: function(preserveSelection) {
             this._preserveSelection = preserveSelection || null;
             this.loader({
                 pageIndex: this.page.index() + 1,
                 pageSize: this.page.sizes.selected(),
-                sortBy: (this.sorting.sortColumn ? this.sorting.sortColumn.sortField : ''),
+                sortBy: (this.sorting.sortColumn ? this.sorting.sortColumn.sortBy : ''),
                 sortDirection: this.sorting.sortOrder
             },
             this._reloadCallback.bind(this),
@@ -151,7 +175,9 @@
         },
 
         restoreSelection: function () {
-            var selection = this.selectedItem(), items = this.items(), newSelection = null;
+            var selection = this.items.selected(),
+                items = this.items(),
+                newSelection = null;
             if (selection) {
                 for (var i = 0; i < items.length; i++) {
                     if (this.comparator(items[i], selection)) {
@@ -164,10 +190,11 @@
         },
 
         selectItem: function selectItem(item) {
-            this.selectedItem(item);
-            if (this.config.selectItem) {
-                this.config.selectItem(item);
+            this.items.selected(item);
+            if (this._settings.selectItem) {
+                this._settings.selectItem(item);
             }
+            return true;
         },
 
         sort: function (column) {
@@ -178,19 +205,28 @@
             this.reload();
         },
 
-        toArray: function(target) {
+        toArray: function (target, values) {
+            if (Array.isArray(target)) {
+                return target;
+            }
+
             var data = ko.unwrap(target || {}),
-                dataArr = [];
-            for (var key in data) {
-                if (data.hasOwnProperty(key) && (data[key] || data[key] === false || data[key] === 0)) {
-                    dataArr.push([data[key], key]);
+                dataArr = [],
+                map = values === 'value'
+                          ? function(key, value) { dataArr.push(value); }
+                          : values === 'key'
+                              ? function(key) { dataArr.push(key); }
+                              : function(key, value) { dataArr.push([key, value]); };
+            for (var prop in data) {
+                if (data.hasOwnProperty(prop) && (data[prop] || data[prop] === false || data[prop] === 0)) {
+                    map(prop, data[prop]);
                 }
             }
             return dataArr;
         },
 
         _staticLoader: function (options, callback, totalCallback) {
-            var items = this.config.items.slice();
+            var items = this._settings.items.slice();
             items.sort(sorter(options.sortBy, options.sortDirection));
             callback(options.pageSize ? items.slice(options.pageSize * (options.pageIndex - 1), options.pageSize * options.pageIndex) : items);
             totalCallback(items.length);
@@ -211,7 +247,13 @@
         },
 
         _defaultComparator: function (a, b) {
-            return a && b && a.id && b.id ? a.id === b.id : a === b;
+            return a && b && a.id && b.id ? a[this._settings.compareBy] === b[this._settings.compareBy] : a === b;
+        },
+
+        _createColumnDefenition: function (column) {
+            column = isString(column) ? { value: column.trim() } : column;
+            column.sortable = column.sortable != null ? column.sortable : this._settings.sortable;
+            return new TableColumn(column);
         }
     });
 
@@ -221,12 +263,12 @@
 
     var templateEngine = new ko.nativeTemplateEngine();
 
-    $('head').append('<style type="text/css">\
-        .header:after {content: "";float: right;margin-top: 7px;visibility: hidden;}\
-        .sortDown:after {border-width: 0 4px 4px;border-style: solid;border-color: #000 transparent;visibility: visible;}\
-        .sortUp:after {border-bottom: none;border-left: 4px solid transparent;border-right: 4px solid transparent;border-top: 4px solid #000;visibility: visible;}\
-        .selectedItem {background-color: #f5f5f5}\
-    </style>');
+    document.getElementsByTagName('head')[0].appendChild(ko.utils.parseHtmlFragment('<style type="text/css">\
+            .dt-header:after {content: "";float: right;margin-top: 7px;visibility: hidden;}\
+            .dt-sort-down:after {border-width: 0 4px 4px;border-style: solid;border-color: #000 transparent;visibility: visible;}\
+            .dt-sort-up:after {border-bottom: none;border-left: 4px solid transparent;border-right: 4px solid transparent;border-top: 4px solid #000;visibility: visible;}\
+            .dt-selected {background-color: #f5f5f5}\
+        </style>')[0]);
 
     templateEngine.addTemplate = function (templateName, templateMarkup) {
         document.write('<script type="text/html" id="' + templateName + '">' + templateMarkup + '<' + '/script>');
@@ -236,17 +278,17 @@
                         <thead>\
                             <tr data-bind="foreach: columns">\
                                <!-- ko if: $data.sortable -->\
-                                   <th class="header" data-bind="text: name, css: cssClass, style: { width: width }, click: $root.sort"></th>\
+                                   <th class="dt-header" data-bind="text: name, css: cssClass, style: { width: width }, click: $root.sort"></th>\
                                <!-- /ko -->\
                                <!-- ko ifnot: $data.sortable -->\
-                                   <th class="header" data-bind="text: name, css: cssClass, style: { width: width }"></th>\
+                                   <th class="dt-header" data-bind="text: name, css: cssClass, style: { width: width }"></th>\
                                <!-- /ko -->\
                             </tr>\
                         </thead>');
 
     templateEngine.addTemplate('ko_table_body', '\
                         <tbody data-bind="foreach: items">\
-                            <tr data-bind="click: $root.selectItem, css: {selectedItem : $root.comparator($root.selectedItem(), $data)}">\
+                            <tr data-bind="click: $root.selectItem, css: {dt-selected : $root.comparator($root.items.selected(), $data)}">\
                                 <!-- ko foreach: $parent.columns -->\
                                     <!-- ko if: template -->\
                                         <td data-bind="template: { name: template, data: typeof value == \'function\' ? value($parent) : $parent[value] }"></td>\
@@ -297,7 +339,7 @@
     </tfoot>');
 
     ko.bindingHandlers.dataTable = {
-        init: function (element, valueAccessor) {
+        init: function () {
             return { 'controlsDescendantBindings': true };
         },
         update: function (element, valueAccessor, allBindingsAccessor) {
@@ -307,9 +349,13 @@
                 tableBodyTemplateName = allBindings.dtBodyTemplate || 'ko_table_body',
                 tablePagerTemplateName = allBindings.dtFooterTemplate || 'ko_table_pager';
 
-            $(element).empty();
+            ko.utils.emptyDomNode(element);
 
             var table = element;
+
+            if (!(viewModel instanceof ko.dataTable.ViewModel)) {
+                viewModel = new ko.dataTable.ViewModel(Array.isArray(ko.unwrap(viewModel)) ? { items: viewModel } : viewModel);
+            }
 
             // Render table header
             var headerContainer = table.appendChild(document.createElement('DIV'));
@@ -322,7 +368,6 @@
             // Render table pager
             var pagerContainer = table.appendChild(document.createElement('DIV'));
             ko.renderTemplate(tablePagerTemplateName, viewModel, { templateEngine: templateEngine }, pagerContainer, 'replaceNode');
-
         }
     };
 })();
